49e478708e99d159675977f1064a48705db4b22d
Jeremy Busk <jeremy@pyrofex.net>
2018-11-16 17:47:42 -0700

    Update install-ubuntu18.04.sh

--- install-ubuntu18.04.sh
+++ install-ubuntu18.04.sh
@@ -10,7 +10,7 @@ sudo apt-get -y install mysql-server
 # chmod 700 /var/lib/mysql-files/
 
 
-sudo apt-get update -y
+sudo apt-get update
 sudo apt-get -y install php7.2 libapache2-mod-php7.2 php7.2-common php7.2-mbstring php7.2-xmlrpc php7.2-soap php7.2-gd php7.2-xml php7.2-intl php7.2-mysql php7.2-cli php7.2-mcrypt php7.2-zip php7.2-curl -y
 
 sudo cp /etc/php/7.2/apache2/php.ini /etc/php/7.2/apache2/php.ini.o
