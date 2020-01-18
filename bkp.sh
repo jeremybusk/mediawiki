0dc471d5f442a7973dd51fe2eae3d3615fe69b25
Jeremy Busk <jeremybusk@gmail.com>
2020-01-18 13:11:48 -0700

    Update bkp.sh

--- bkp.sh
+++ bkp.sh
@@ -2,15 +2,22 @@
 set -e
 # Simple example backup/update scripts
 
+bkp_dir="/bkp"
+html_folder="/var/www/html"
 timestamp=$(date "+%Y%m%dT%H%M%S")
 sql_bkp_file="/bkp/wiki.sql.${timestamp}"
 html_bkp_file="/bkp/wiki-html.${timestamp}.tgz"
-html_folder="/var/www/html"
 
-mkdir -p /bkp
+
+mkdir -p ${bkp_dir}
+cd ${bkp_dir}
+
+
 mysqldump --user=wiki --password=wiki wiki > ${sql_bkp_file}
 tar -zcvf ${html_bkp_file} ${html_folder}
 tar -ztvf ${html_bkp_file}
+
+
 cd ${html_folder}
 composer update --no-dev
 wget https://releases.wikimedia.org/mediawiki/1.34/mediawiki-1.34.0.tar.gz
@@ -19,6 +26,8 @@ rm mediawiki-1.34.0.tar.gz
 cd maintenance
 php update.php
 cd ../
+
+
 find . -type f -exec chmod 644 {} \;
 find . -type d -exec chmod 755 {} \;
 echo "Mediawiki update complete."
